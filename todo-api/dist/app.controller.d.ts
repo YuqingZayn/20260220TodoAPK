2b336d48f52fce0de8b9a642dd8ced9de1cc9150
YuqingZayn <1961647257zzp@gmail.com>
2026-02-20 16:24:13 +0800

    Fix: definitively remove dist from git tracking

--- todo-api/dist/app.controller.d.ts
+++
@@ -1,6 +0,0 @@
-export declare class AppController {
-    getHello(): {
-        message: string;
-        version: string;
-    };
-}
--- todo-api/dist/modules/todos/todos.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateTodoDto {
-    title: string;
-    priority?: number;
-}
-export declare class UpdateTodoDto {
-    title?: string;
-    completed?: boolean;
-    priority?: number;
-}
